e5492ae20591a81891db2471796569fcbc3c4759
Prosperugonna <prosperugonna@gmail.com>
2023-06-21 06:56:51 -0700

    updated

--- models/state.py
+++ models/state.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-""" holds class City"""
+""" holds class State"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
 
 
-class City(BaseModel, Base):
-    """Representation of city """
+class State(BaseModel, Base):
+    """Representation of state """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
+        __tablename__ = 'states'
         name = Column(String(128),
                       nullable=False)
-        state_id = Column(String(60),
-                          ForeignKey('states.id'),
-                          nullable=False)
-        places = relationship("Place",
-                              backref="cities",
-                              cascade="all, delete-orphan")
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
     else:
         name = ""
-        state_id = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """initializes state"""
         super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """fs getter attribute that returns City instances"""
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
