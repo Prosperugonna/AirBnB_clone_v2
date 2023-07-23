bea1560c20f77c997cb84da0fdbdd86018ba6f63
Prosperugonna <prosperugonna@gmail.com>
2023-07-23 03:31:42 -0700

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,12 +15,8 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    __classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-
+    # Existing __classes dictionary...
+    
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
@@ -32,31 +28,7 @@ class FileStorage:
         else:
             return self.__objects
 
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] \
-                            = FileStorage.__classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+    # Existing new(), save(), and reload() methods...
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
