5ff5cc3f54382ba13e225eda8afac3efc7100564
Prosperugonna <prosperugonna@gmail.com>
2023-08-15 12:26:42 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,12 @@
 """Run a flask app that returns Hello HBNB"""
 from flask import Flask
 
-
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello():
-    """return hello NMBB"""
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_airbnb():
+    """Return hello NMBB for /airbnb-onepage/"""
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
